cbba7bfa0912f81578eaacb2bbb7c0797452ca62
Hinanshi Suthar <88201664+hinanshisuthar@users.noreply.github.com>
2021-10-21 14:07:41 +0530

    refactor : arrow functions
    DRY implemented

--- script.js
+++ script.js
@@ -5,47 +5,44 @@ var buttonSwitch = document.querySelector("#switch-button");
 var enterText = document.querySelector("#enter-text");
 var outputText = document.querySelector("#output-text");
 
-
 var serverURl1 = "https://api.funtranslations.com/translate/us2uk.json";
-
 var serverURl2 = "https://api.funtranslations.com/translate/uk2us.json";
 
-function getTranslationURl1(text){
-    return serverURl1 + "?" + "text=" + text;
+// function getTranslationURl1(text) {
+//     return serverURl1 + "?" + "text=" + text;
+// }
+
+// function getTranslationURl2(text) {
+//     return serverURl2 + "?" + "text=" + text;
+// }
+
+function getTranslationURl(url, text) {
+    return url + "?" + "text=" + text;
 }
 
-function getTranslationURl2(text){
-    return serverURl2 + "?" + "text=" + text;
-}
-
-function errorHandler(error){
-    console.log("error occured", error)
-    alert("something is wrong with the server! try again after some time");
-}
-
-function clickedEventHandler(){
+buttontranslate.addEventListener("click", () => {
     var inputText = textArea.value;
     
-    fetch(getTranslationURl1(inputText))
+    fetch(getTranslationURl(serverURl1, inputText))
     .then(response => response.json())
     .then(json => outputDiv.innerText=json.contents.translated)
-    .catch(errorHandler); 
+    .catch(error => {
+    console.log("error occured", error)
+    alert("something is wrong with the server! try again after some time"); 
 
-};
+});
 
-function clickedEventHandlerSwitch(){
+buttonSwitch.addEventListener("click", () => {
     var inputText = textArea.value;
 
     // enterText.innerHTML="Enter UK text below";
     // outputText.innerHTML="Converted US text is below";
 
-    fetch(getTranslationURl2(inputText))
+    fetch(getTranslationURl2(serverURl2, inputText))
     .then(response => response.json())
     .then(json => outputDiv.innerText=json.contents.translated)
-    .catch(errorHandler);
-
-}
-
-buttontranslate.addEventListener("click", clickedEventHandler);
-
-buttonSwitch.addEventListener("click", clickedEventHandlerSwitch);
+    .catch(error => {
+    console.log("error occured", error)
+    alert("something is wrong with the server! try again after some time");
+})
+});
